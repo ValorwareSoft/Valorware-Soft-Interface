f9eaf655739d37bf38e720392f244d23fd83d42e
Valorware Soft <valorwaresoftceo@gmail.com>
2024-05-21 08:26:52 +0530

    chnages

---
+++ src/app/modules/pop-up/pop-up.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-pop-up',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pop-up.component.html',
+  styleUrls: ['./pop-up.component.scss']
+})
+export class PopUpComponent {
+
+}
